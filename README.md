6dc13b70892b75efdc7e07969ad89d78294ae40b
Zephyr <zhentaop118@gmail.com>
2025-02-25 20:51:28 -0500

    added requirements.txt and demo website to scrap; updated the roadmap

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 
 Here's the idea:
-
-1. Load comic from a given website to the reader
-2. Have option to download
-3. Have libraries
+1. The reader will be hosted in a website
+2. Load comic from a given website to the reader
+3. Have option to download
+4. Have libraries
 
 AI related ideas: 
 
@@ -14,8 +14,12 @@ AI related ideas:
    and the ai will generate another image with user's taste
 5. Have the ai narrate the comic via image recognition
 6. Image to animation through use of ai 
-7. Using ai in get higher qualit
+7. Using ai in get higher image qualities
 
 ## Roadmap for loading comic
 
-1. Before the users choose their comic, we need to have right configurations to request information from websites
+1. Before the users choose their comic, we need to have right configurations to request information from websites, those should be stored in json
+2. When a website has many comics, then each of those comic element should be preserved so that it can be loaded as a whole in the website
+3. The website html will be the container
+4. Inside the container will contain comic elements. Then CSS can be used to organize the layout of the comic
+5. 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from bs4 import BeautifulSoup
-import requests
+import requests                    
+import json   
+from bs4 import BeautifulSoup   
+
 
 
 # what are the steps to retrieving comic?
@@ -7,23 +9,30 @@ import requests
 # so there must be packages for different websites. The website will include 
 
 
-def get_website(link: str):
-    """Loads the website given the link of the website."""
+def get_website(url: str) -> BeautifulSoup:
+    """Get the raw html content of the website"""
 
-    pass
+    response = requests.get(url).text                   # request raw html texts for beautifulsoup to parse
+    soup = BeautifulSoup(response, "html.parser")       # parse html; can't request raw html alone
+    
+    return soup
+
 
 def get_comic():
-    """Get a comic from the website"""
+    """Get a comic from the website, should be the element displayed in their website"""
     pass
 
 def get_chapters():
     """Load chapters from the website"""
     pass
 
+def download_chapters():
+    """Downloads the chapter and store in some directory"""
 
 
 def main():
-
+    solo_leveling = {"url" : "https://w5.solo-leveling-manga.com/", "multiple_comics" : False}
+    print(type(get_website(solo_leveling["url"])))
     pass
 
 
